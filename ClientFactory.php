dd284dc8d1da532128184b04360cc8d93eee307e
andig <cpuidle@gmx.de>
2016-07-17 17:27:03 +0200

    Refactored retry functions

--- ClientFactory.php
+++ ClientFactory.php
@@ -17,12 +17,38 @@ use Doctrine\Common\Cache\FilesystemCache;
 
 define('CACHE_DURATION', 3600 * 24 * 365);
 
-function retryDecider() {
-	return function (
-	  $retries,
-	  Request $request,
-	  Response $response = null,
-	  RequestException $exception = null
+class ClientFactory
+{
+	public static function getClient($clientOptions = []) {
+		// Basic directory cache example
+		$cacheProvider = new FilesystemCache(__DIR__ . '/cache');
+		// Guzzle will determine an appropriate default handler if `null` is given.
+		$defaultHandler = null;
+		// Create a cache handler with a given cache provider and default handler.
+		$cacheHandler = new CacheHandler($cacheProvider, $defaultHandler, [
+			'methods' => ['GET'],
+			'expire' => CACHE_DURATION,
+		]);
+
+		$handlerStack = HandlerStack::create($cacheHandler);
+		$handlerStack->push(Middleware::retry(__CLASS__.'::retryDecider', __CLASS__.'::retryDelay'));
+
+		$options = array_merge([
+			'handler' => $handlerStack,
+			'timeout'  => 10,
+			// 'allow_redirects' => false,
+		], $clientOptions);
+
+		$client = new Client($options);
+
+		return $client;
+	}
+
+	static function retryDecider(
+		  $retries,
+		  Request $request,
+		  Response $response = null,
+		  RequestException $exception = null
 	) {
 		// Limit the number of retries to 5
 		if ($retries >= 5) {
@@ -43,7 +69,6 @@ function retryDecider() {
 			// Retry on rate limits
 			if ($response->getStatusCode() == 429) {
 				$retryDelay = $response->getHeaderLine('Retry-After');
-				var_dump($retryDelay);
 
 				if (strlen($retryDelay)) {
 					sleep((int)$retryDelay);
@@ -53,39 +78,9 @@ function retryDecider() {
 		}
 
 		return false;
-	};
-}
+	}
 
-function retryDelay() {
-    return function($numberOfRetries) {
-        return 1000 * $numberOfRetries;
-    };
-}
-
-class ClientFactory
-{
-	public static function getClient($clientOptions = []) {
-		// Basic directory cache example
-		$cacheProvider = new FilesystemCache(__DIR__ . '/cache');
-		// Guzzle will determine an appropriate default handler if `null` is given.
-		$defaultHandler = null;
-		// Create a cache handler with a given cache provider and default handler.
-		$cacheHandler = new CacheHandler($cacheProvider, $defaultHandler, [
-			'methods' => ['GET'],
-			'expire' => CACHE_DURATION,
-		]);
-
-		$handlerStack = HandlerStack::create($cacheHandler);
-		$handlerStack->push(Middleware::retry(retryDecider(), retryDelay()));
-
-		$options = array_merge([
-			'handler' => $handlerStack,
-			'timeout'  => 10,
-			// 'allow_redirects' => false,
-		], $clientOptions);
-
-		$client = new Client($options);
-
-		return $client;
+	static function retryDelay($numberOfRetries) {
+		return 1000 * $numberOfRetries;
 	}
 }
