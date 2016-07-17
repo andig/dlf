7189e0fedd6d91b2437178e66cff7fa540e4a7d8
andig <cpuidle@gmx.de>
2016-07-17 17:16:34 +0200

    Merged

--- credentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('CLIENT_ID', '9416f48e96bb4fadb826fc94fed34ebb');
-define('CLIENT_SECRET', 'a4818f656539430ab21c5f5778ae4dee');
-define('REDIRECT_URI', 'http://localhost:9042');
-define('REDIRECT_URI_INTERACTIVE', 'http://localhost/dlf');
